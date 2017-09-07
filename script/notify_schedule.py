e378d8afd14062d2aeab93e375374acaaad0f75d
Matthias Klass <matthias.klass@tngtech.com>
2017-09-07 20:23:16 +0200

    Weißwurst breakfast starts earlier

---
+++ script/notify_schedule.py
@@ -0,0 +1,45 @@
+import requests
+import datetime
+
+
+def calculateTimeToLookForInSchedule(date):
+    minute = date.minute
+    hour = date.hour
+    if minute == 0:
+        return formatTime(hour, 0)
+    elif minute > 30:
+        return formatTime(hour + 1, 0)
+    else:
+        return formatTime(hour, 30)
+
+
+def sendNotificationToSlackFor(entries):
+    text = reduce(lambda x, y: x + '\n' + y,
+                  map(lambda x: x['loc'][0] + ": " + x['title'], entries)
+                  )
+    text = "*Now starting*:\n" + text
+    requests.post('https://hooks.slack.com/services/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',
+                      data='{"text":"' + text + '"}')
+
+
+def formatTime(hour, minute):
+    return '{num:02d}'.format(num=hour) + ':' + '{num:02d}'.format(num=minute)
+
+
+now = datetime.datetime.now()
+r = requests.get('http://swacamp.org/schedule/program.json')
+
+minute = now.minute
+timeToLookFor = calculateTimeToLookForInSchedule(now)
+dateToLookFor = datetime.date.today()
+
+dateToLookFor = "2017-09-09"
+
+
+json = r.json()
+forDay = filter(lambda x: x['date'] == dateToLookFor, json)
+forTime = filter(lambda x: x['time'] == timeToLookFor, forDay)
+
+print dateToLookFor + " " + timeToLookFor + ": found:" + str(forTime)
+if len(forTime) > 0:
+    sendNotificationToSlackFor(forTime)

2bce6acd4dc4092b140b0e011795f1112202a630
Matthias Klass <matthias.klass@tngtech.com>
2018-05-05 00:50:10 +0200

    prefent null dereference, code formatting

--- site.js
+++ site.js
@@ -1,124 +1,127 @@
-
 $.extend($.easing,
-{
+  {
     def: 'easeOutQuad',
     easeInOutExpo: function (x, t, b, c, d) {
-        if (t==0) return b;
-        if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+      if (t == 0) return b;
+      if (t == d) return b + c;
+      if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+      return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
     }
-});
+  });
 
-(function( $ ) {
+(function ($) {
 
-    var settings;
-    var disableScrollFn = false;
-    var navItems;
-    var navs = {}, sections = {};
+  var settings;
+  var disableScrollFn = false;
+  var navItems;
+  var navs = {}, sections = {};
 
-    $.fn.navScroller = function(options) {
-        settings = $.extend({
-            scrollToOffset: 170,
-            scrollSpeed: 800,
-            activateParentNode: true
-        }, options );
-        navItems = this;
+  $.fn.navScroller = function (options) {
+    settings = $.extend({
+      scrollToOffset: 170,
+      scrollSpeed: 800,
+      activateParentNode: true
+    }, options);
+    navItems = this;
 
-        //attatch click listeners
-    	navItems.on('click', function(event){
-            var navID = $(this).attr("href").substring(1);
-            if (navID.length === 0) {
-                return true;
-            }
-            event.preventDefault();
-            disableScrollFn = true;
-            activateNav(navID);
-            populateDestinations(); //recalculate these!
-        	$('html,body').animate({scrollTop: sections[navID] - settings.scrollToOffset},
-                settings.scrollSpeed, "easeInOutExpo", function(){
-                    disableScrollFn = false;
-                }
-            );
-    	});
+    //attatch click listeners
+    navItems.on('click', function (event) {
+      var navID = $(this).attr("href").substring(1);
+      if (navID.length === 0) {
+        return true;
+      }
+      event.preventDefault();
+      disableScrollFn = true;
+      activateNav(navID);
+      populateDestinations(); //recalculate these!
+      $('html,body').animate({scrollTop: sections[navID] - settings.scrollToOffset},
+        settings.scrollSpeed, "easeInOutExpo", function () {
+          disableScrollFn = false;
+        }
+      );
+    });
 
-        //populate lookup of clicable elements and destination sections
-        populateDestinations(); //should also be run on browser resize, btw
+    //populate lookup of clicable elements and destination sections
+    populateDestinations(); //should also be run on browser resize, btw
 
-        // setup scroll listener
-        $(document).scroll(function(){
-            if (disableScrollFn) { return; }
-            var page_height = $(window).height();
-            var pos = $(this).scrollTop();
-            for (i in sections) {
-                if ((pos + settings.scrollToOffset >= sections[i]) && sections[i] < pos + page_height){
-                    activateNav(i);
-                }
-            }
-        });
-    };
+    // setup scroll listener
+    $(document).scroll(function () {
+      if (disableScrollFn) {
+        return;
+      }
+      var page_height = $(window).height();
+      var pos = $(this).scrollTop();
+      for (i in sections) {
+        if ((pos + settings.scrollToOffset >= sections[i]) && sections[i] < pos + page_height) {
+          activateNav(i);
+        }
+      }
+    });
+  };
 
-    function populateDestinations() {
-        navItems.each(function(){
-            var scrollID = $(this).attr('href').substring(1);
-            if (scrollID.length === 0) {
-                return;
-            }
-            navs[scrollID] = (settings.activateParentNode)? this.parentNode : this;
-            sections[scrollID] = $(document.getElementById(scrollID)).offset().top;
-        });
+  function populateDestinations() {
+    navItems.each(function () {
+      var scrollID = $(this).attr('href').substring(1);
+      if (scrollID.length === 0) {
+        return;
+      }
+      navs[scrollID] = (settings.activateParentNode) ? this.parentNode : this;
+      sections[scrollID] = $(document.getElementById(scrollID)).offset().top;
+    });
+  }
+
+  function activateNav(navID) {
+    for (nav in navs) {
+      $(navs[nav]).removeClass('active');
     }
-
-    function activateNav(navID) {
-        for (nav in navs) { $(navs[nav]).removeClass('active'); }
-        $(navs[navID]).addClass('active');
-    }
-})( jQuery );
+    $(navs[navID]).addClass('active');
+  }
+})(jQuery);
 
 
 function evaluateLanguage() {
-    var lang = $.urlParam("lang");
-    if (lang === 'de') {
-        $(".en").hide();
-    } else { // en
-        $(".de").hide();
-    }
+  var lang = $.urlParam("lang");
+  if (lang === 'de') {
+    $(".en").hide();
+  } else { // en
+    $(".de").hide();
+  }
 }
 
-function toggleLanguage()
-{
-    $(".de").toggle();
-    $(".en").toggle();
+function toggleLanguage() {
+  $(".de").toggle();
+  $(".en").toggle();
 }
 
 $.urlParam = function (name) {
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)')
-        .exec(window.location.href);
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)')
+    .exec(window.location.href);
 
-    return results ? results[1] || 0 : 0;
+  return results ? results[1] || 0 : 0;
 };
 
-$(document).ready(function (){
+$(document).ready(function () {
 
-    evaluateLanguage();
-    $('nav li a').navScroller();
+  evaluateLanguage();
+  $('nav li a').navScroller();
 
-    //section divider icon click gently scrolls to reveal the section
-	$(".sectiondivider").on('click', function(event) {
-    	$('html,body').animate({scrollTop: $(event.target.parentNode).offset().top - 50}, 400, "linear");
-	});
+  //section divider icon click gently scrolls to reveal the section
+  $(".sectiondivider").on('click', function (event) {
+    $('html,body').animate({scrollTop: $(event.target.parentNode).offset().top - 50}, 400, "linear");
+  });
 
-    //links going to other sections nicely scroll
-	$(".container a").each(function(){
-        if ($(this).attr("href").charAt(0) == '#'){
-            $(this).on('click', function(event) {
-        		event.preventDefault();
-                var target = $(event.target).closest("a");
-                var targetHight =  $(target.attr("href")).offset().top
-            	$('html,body').animate({scrollTop: targetHight - 170}, 800, "easeInOutExpo");
-            });
-        }
-	});
+  //links going to other sections nicely scroll
+  $(".container a").each(function () {
+    var href = $(this).attr("href");
+    if (href && href.charAt(0) == '#') {
+      $(this).on('click', function (event) {
+        event.preventDefault();
+        var target = $(event.target).closest("a");
+        var targetHight = $(target.attr("href")).offset().top
+        $('html,body').animate({scrollTop: targetHight - 170}, 800, "easeInOutExpo");
+      });
+    }
+  });
 
 });
 

fa24a6056a0eca66edc2690eb67eb166c0091bce
Peter Gafert <peter.gafert@tngtech.com>
2019-09-09 23:35:50 +0200

    Fixed vulnerability in nokogiri < 1.10.4

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-docker run -i -t -v $(pwd)/:/home/jenkins -p 4000:4000 praqma/gh-pages jekyll serve --watch --host=0.0.0.0
+docker run -i -t -v "$(pwd)"/:/home/jenkins -p 4000:4000 praqma/gh-pages jekyll serve --watch --host=0.0.0.0
